b291478ca86da9e551213a5939ae0eafad6a463c
serakoi <admin@seer-software.com>
2021-12-24 11:09:15 +0100

    init

---
+++ js/src/admin/index.js
@@ -0,0 +1,11 @@
+import app from 'flarum/app';
+
+app.initializers.add('serakoi/flarumstaffbadge', () => {
+  app.extensionData.for('serakoi-flarumstaffbadge').registerSetting({
+    setting: 'serakoi-flarumstaffbadge.staffBadge',
+    name: 'staffBadge',
+    type: 'text',
+    label: app.translator.trans('serakoi-flarumstaffbadge.admin.staffBadge.label'),
+    help: app.translator.trans('serakoi-flarumstaffbadge.admin.staffBadge.help'),
+  });
+});
---
+++ js/src/forum/index.js
@@ -0,0 +1,12 @@
+import { extend } from 'flarum/extend';
+import app from 'flarum/app';
+
+extend(EditUserModal.prototype, 'fields', function (items) {
+    items.add('hasbadge',
+        <div className="Form-group">
+            <label>{app.translator.trans('serakoi-flarumstaffbadge.forum.edit_user.heading')}</label>
+            <input className="FormControl"
+                placeholder={extractText(app.translator.trans('serakoi-flarumstaffbadge.forum.edit_user.placeholder'))}
+                bidi="test" />
+        </div>, 100)
+});
