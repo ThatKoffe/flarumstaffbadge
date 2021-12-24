98ed1bc4bc1764c7539f0ecd56b2c0ed2fa955ee
serakoi <admin@seer-software.com>
2021-12-24 12:44:26 +0100

    Fix Bugs

--- migrations/StaffBadge_column.php
+++ migrations/StaffBadge_column.php
@@ -1,13 +1,13 @@
 <?php
 
-use Illuminate/Database/Schema/Blueprint;
-use Illuminate/Database/Schema/Builder;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
         if (!$schema->hasColumn('users', 'staffBadge')) {
-            $schema->table('users', function (Blueprint $table) {
-                $table->string('staffBadge')->after('username')->index()->nullable();
+            $schema->table('users', function (Blueprint $table) use ($schema) {
+                $table->string('staffBadge', 150)->after('username')->index()->nullable();
             });
         }   
     },
