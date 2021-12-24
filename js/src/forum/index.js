9f32b9e54c4ecd5782952238f9ba1c943b783e19
serakoi <admin@seer-software.com>
2021-12-24 13:35:36 +0100

    tests

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -11,7 +11,7 @@ app.initializers.add('serakoi/flarumstaffbadge', () => {
     User.prototype.staffBadge = Model.attribute('staffBadge');
 
     extend(EditUserModal.prototype, 'oninit', function () {
-        this.status = Stream(user.staffBadge());
+        this.status = Stream(this.attrs.user.staffBadge);
     });
 
     extend(EditUserModal.prototype, 'fields', function (items) {
