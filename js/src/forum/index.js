1c8b0c5f19ce00fb90fcf74b23c8d5417ec0c52e
serakoi <admin@seer-software.com>
2021-12-24 11:27:11 +0100

    D-I

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -4,14 +4,19 @@ import EditUserModal from 'flarum/common/components/EditUserModal';
 import Model from 'flarum/common/Model';
 import User from 'flarum/common/models/User';
 import extractText from 'flarum/common/utils/extractText';
+import Stream from 'flarum/common/utils/Stream';
 import Button from 'flarum/common/components/Button';
 
-extend(EditUserModal.prototype, 'fields', function (items) {
-    items.add('hasbadge',
-        <div className="Form-group">
-            <label>{app.translator.trans('serakoi-flarumstaffbadge.forum.edit_user.heading')}</label>
-            <input className="FormControl"
-                placeholder={extractText(app.translator.trans('serakoi-flarumstaffbadge.forum.edit_user.placeholder'))}
-                bidi="test" />
-        </div>, 100)
+app.initializers.add('serakoi/flarumstaffbadge', () => {
+    this.status = Stream('NaN')
+
+    extend(EditUserModal.prototype, 'fields', function (items) {
+        items.add('hasbadge',
+            <div className="Form-group">
+                <label>{app.translator.trans('serakoi-flarumstaffbadge.forum.edit_user.heading')}</label>
+                <input className="FormControl"
+                    placeholder={extractText(app.translator.trans('serakoi-flarumstaffbadge.forum.edit_user.placeholder'))}
+                    bidi={this.status} />
+            </div>, 100)
+    });
 });
