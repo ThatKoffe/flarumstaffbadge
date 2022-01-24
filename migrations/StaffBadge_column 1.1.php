dc3bdfc16709f421938ef0644435ffd040eed93f
serakoi <admin@seer-software.com>
2022-01-24 12:56:26 +0100

    Fix migration?

--- migrations/StaffBadge_column 1.1.php
+++ migrations/StaffBadge_column 1.1.php
@@ -5,11 +5,6 @@ use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        if (!$schema->hasColumn('users', 'staffBadge')) {
-            $schema->table('users', function (Blueprint $table) use ($schema) {
-                $table->string('staffBadge', 150)->after('username')->index()->nullable();
-            });
-        }  
         if (!$schema->hasColumn('users', 'tagList')) {
             $schema->table('users', function (Blueprint $table) use ($schema) {
                 $table->string('tagList', 150)->after('username')->index()->nullable();
@@ -19,7 +14,6 @@ return [
 
     'down' => function (Builder $schema) {
         $schema->table('users', function (Blueprint $table) use ($schema) {
-            $table->dropColumn('staffBadge');
             $table->dropColumn('tagList');
         });
     },
